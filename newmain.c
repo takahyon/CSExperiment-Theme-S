0793ada8088b38e904fa9e0d7c396349ee259ebc
takahyon <takahyon@gmail.com>
2017-10-17 15:10:10 +0900

    1st Commit "C"

---
+++ newmain.c
@@ -0,0 +1,42 @@
+#include "p30f3012.h"
+
+_FOSC(CSW_FSCM_OFF & FRC); // Internal 7.37MHz clock
+_FWDT(WDT_OFF); // Watchdog Timer OFF
+_FBORPOR(MCLR_DIS & PBOR_OFF & BORV27 & PWRT_4); // Reset Condition
+_FGS(CODE_PROT_OFF & GWRP_OFF); // Code protection OFF
+
+
+
+
+void Wait1S(void)
+// Just wait for a single second (really?)
+{
+    int i, j;
+
+    for (i = 0; i < 100; i++) {
+        
+        for (j = 0; j < 2302; j++) {
+        }
+    }
+    
+}
+
+
+int main(void) {
+    /** /TRISB = 0x0000; // RB7 - RB0 all for output
+    LATB = 0x0000; // RB7 - RB0 output '0'
+    ADPCFG = 0x00ff; // RB7 - RB0 digital input enabled
+    TRISC = 0x0000; // RC13 - RC15 all for output
+    LATC = 0x0000; // RC13 - RC15 output '0'
+    TRISD = 0x0001; // RD0 for input
+*/
+    while(1){
+  
+        Wait1S();
+    }
+      
+    
+    return 0;
+}
+
+
---
+++ newmain1.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   newmain1.c
+ * Author: takamasa
+ *
+ * Created on 2017/10/03, 14:01
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
